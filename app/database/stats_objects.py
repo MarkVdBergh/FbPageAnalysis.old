364a2a5ad02ac1b34c31b6a3abf8785908a8c5c5
vandenbergh.mark@gmail.com <MarkVdBergh>
2017-02-04 02:56:43 +0100

    Work on stats_objects

--- app/database/stats_objects.py
+++ app/database/stats_objects.py
@@ -1,5 +1,20 @@
+from mongoengine import register_connection, Document
+
+from app.settings import TESTING_DB, PRODUCTION_DB
+
+register_connection(alias='test', name=TESTING_DB)
+register_connection(alias='default', name=PRODUCTION_DB)
+
 # ToDo: mongo $size can't query size of lists (see: https://docs.mongodb.com/manual/reference/operator/query/size/#_S_size)
 #      workaround: "create a counter field that you increment when you add elements to a field."
 # ToDo: Add language field to documents
 
+class Users(Document):
+    pass
+
+class Pages(Document):
+    pass
+
+class PostStast(Document):
+    pass
 
