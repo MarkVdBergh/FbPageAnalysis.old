fdffbef611bf3a33515125e0570ac542d2094f62
vandenbergh.mark@gmail.com <MarkVdBergh>
2017-02-05 06:20:50 +0100

    MongoDb schema design
    - Work on stats_objects

--- app/database/stats_objects.py
+++ app/database/stats_objects.py
@@ -1,12 +1,12 @@
-from mongoengine import StringField, BooleanField, URLField, register_connection, Document, ListField, ReferenceField
+from datetime import datetime
+
+import pytz
+from mongoengine import StringField, BooleanField, URLField, register_connection, Document, ListField, ReferenceField, ObjectIdField, DateTimeField, IntField
 
 from app.settings import TESTING_DB, PRODUCTION_DB
 
 register_connection(alias='test', name=TESTING_DB)
 
-# ToDo: mongo $size can't query size of lists (see: https://docs.mongodb.com/manual/reference/operator/query/size/#_S_size)
-#      workaround: "create a counter field that you increment when you add elements to a field."
-# ToDo: Add language field to documents
 """
         Get a set of posts via FbPosts. Iterate each post and save processed data to the appropriate collections.
 
@@ -16,31 +16,104 @@ register_connection(alias='test', name=TESTING_DB)
             - all text, messages, comments for a user, page during a period
             - average reactions, shares, comments during a period
             - histogram reactons, shares, comments
-            -
-
 """
 
 register_connection(alias='default', name=PRODUCTION_DB)
 
 
+class Pages(Document):
+    meta = {'collection': 'facebook', 'indexes': ['pageid', '$name', 'users']}
+
+    id_ = ObjectIdField(db_field='_id', primary_key=True)
+    page_id = StringField(db_field='pageid')
+    name = StringField()
+    posts = ListField(ReferenceField('Users'))  # List of 'Users' that created, reacted, shared or commented on a post
+
+    @classmethod
+    def extract_page(cls, fbp):
+        p = Pages()
+        p.page_id = fbp.profile.id
+        p.name = fbp.profile.name
+        return cls.id_
+
+    def __unicode__(self):
+        return self.to_json()
+
+
 class Users(Document):
-    id = StringField()
+    id_ = ObjectIdField(db_field='_id', primary_key=True)
+    userid = StringField()
     name = StringField()
     picture = URLField()
     is_silhouette = BooleanField()
     link = URLField()
     pages_active = ListField(ReferenceField('Pages'))  # List of 'Pages' the user creacted, shared or commented on
 
+    def extract_users(self, fbp):
+        from_user = Users(userid=fbp.pro)
 
-class Pages(Document):
-    id = StringField()
-    name = StringField()
-    users_active = ListField(ReferenceField('Users'))  # List of 'Users' that created, reacted, shared or commented on a post
+        return self.id_
 
-
-class PostStats(Document):
-    pass
+    def __unicode__(self):
+        return self.to_json()
 
 
-class PostTexts(Document):
-    pass
+class PostStat(Document):
+    id_ = ObjectIdField(db_field='_id', primary_key=True)
+
+    shares = IntField()
+    reactions = ListField(ReferenceField(Texts))
+
+    type = StringField()
+    status_type = StringField()
+
+    def build_(self, fb_post):
+        return self.id_
+
+    def __unicode__(self):
+        return self.to_json()
+
+
+class Texts(Document):
+    id_ = ObjectIdField(db_field='_id', primary_key=True)
+
+    language = StringField(default='nl')
+
+    def build_(self, fb_post):
+        return self.id_
+
+    def __unicode__(self):
+        return self.to_json()
+
+
+class Posts(Document):
+    id_ = ObjectIdField(db_field='_id', primary_key=True)
+    updated = DateTimeField(datetime.utcnow())
+
+    created = DateTimeField()
+    post_id = StringField(db_field='id', required=True)
+
+    # Reference fields
+    page_id = ReferenceField(Pages)
+
+    from_user = ReferenceField(Users)
+    to_user = ListField(ReferenceField(Users))  # Facebook users mentioned in message
+
+    message = ReferenceField(Texts)
+    story = ReferenceField(Texts)
+    post_link = URLField()
+    name = ReferenceField(Texts)
+    picture = URLField()
+    comments = ListField(ReferenceField(Texts))
+
+    def build_post(self, fbp):
+        self.created = datetime.fromtimestamp(fbp.created_time, tz=pytz.utc)
+        self.post_id = fbp.postid
+
+        self.page_id = Pages.extract_page(fbp)
+        self.from_user = Users().extract_users(fbp)
+
+        return self.id_
+
+    def __unicode__(self):
+        return self.to_json()
